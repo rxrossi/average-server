75bdbd3edc6bcabddd452d9fbdf8f036e598bb7a
rxrossi <rx.rossi@gmail.com>
2018-04-30 09:30:39 -0300

    refactor on files route

---
+++ app/files/routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const storage = path.join(process.cwd(), `fileStorage`);
+
+module.exports = setupFileStorageRoutes;
+
+function setupFileStorageRoutes(app, route = "/files") {
+  app.use(fileUpload());
+
+  app.post(route, (req, res) => {
+    if (!req.files) {
+      return res.status(422).json({
+        error: {
+          message: "No files receive"
+        }
+      });
+    }
+    const file = req.files.file;
+    const fileName = Date.now() + file.name;
+
+    //move the file
+    const filePath = path.join(storage, fileName);
+
+    file.mv(filePath, err => {
+      if (err) {
+        console.error("err", err);
+        return res.status(500).json(err);
+      }
+    });
+
+    res.json({
+      response: {
+        storedAt: route + "/" + fileName
+      }
+    });
+  });
+
+  app.use(route, express.static(storage));
+}
--- app/index.js
+++ app/index.js
@@ -1,52 +1,20 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
-
-const storage = path.join(process.cwd(), `fileStorage`);
+const setupFileStorageRoutes = require("./files/routes");
 
 app.use(bodyParser.json());
 // app.use(morgan("tiny"));
 app.use(cors());
-app.use(fileUpload());
+
+setupFileStorageRoutes(app, "/files");
 
 app.get("/", (req, res) => {
   res.sendStatus(200);
 });
-
-app.post("/files", (req, res) => {
-  if (!req.files) {
-    return res.status(422).json({
-      error: {
-        message: "No files receive"
-      }
-    });
-  }
-  const file = req.files.file;
-  const fileName = Date.now() + file.name;
-
-  //move the file
-  const filePath = path.join(storage, fileName);
-
-  file.mv(filePath, err => {
-    if (err) {
-      console.error("err", err);
-      return res.status(500).json(err);
-    }
-  });
-
-  res.json({
-    response: {
-      storedAt: "/files/" + fileName
-    }
-  });
-});
-
-app.use("/files", express.static(storage));
 
 app.use("/users", require("./users/routes"));
 
