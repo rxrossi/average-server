6caa08953be84e0559b30db91d0250af5770d287
rxrossi <rx.rossi@gmail.com>
2018-05-04 13:41:47 -0300

    Articles now returns .id property

--- app/articles/model/index.js
+++ app/articles/model/index.js
@@ -1,39 +1,55 @@
 const mongoose = require("mongoose");
 var uniqueValidator = require("mongoose-unique-validator");
 
-const schema = mongoose.Schema({
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  content: {
-    type: String,
-    required: true
+var schemaOptions = {
+  toObject: {
+    virtuals: true
   },
-  tags: [String],
-  published: {
-    type: Boolean,
-    required: true
-  },
-  mainImg: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  creationDate: {
-    type: mongoose.Schema.Types.Date,
-    default: Date.now()
+  toJSON: {
+    virtuals: true
   }
-});
+};
+
+const schema = mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    tags: [String],
+    published: {
+      type: Boolean,
+      required: true
+    },
+    mainImg: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    creationDate: {
+      type: mongoose.Schema.Types.Date,
+      default: Date.now()
+    }
+  },
+  schemaOptions
+);
 
 schema.plugin(uniqueValidator);
 const Article = mongoose.model("Article", schema);
