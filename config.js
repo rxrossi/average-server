7a4979315152be96518883d1c20c472a1e8464c3
rxrossi <rx.rossi@gmail.com>
2018-05-22 08:33:18 -0300

    fix on config file to work with heroku

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
-
-module.exports = {
-  PORT: process.env.PORT,
-  HOST: process.env.HOST,
-  MONGODB: process.env.MONGODB,
-  MONGODB_TEST: process.env.MONGODB_TEST,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_ISS: process.env.JWT_ISS
-};
+try {
+  require('dotenv').config()
+} catch (e) {
+  console.log(e)
+} finally {
+  module.exports = {
+    PORT: process.env.PORT,
+    HOST: process.env.HOST,
+    MONGODB: process.env.MONGODB,
+    MONGODB_TEST: process.env.MONGODB_TEST,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_ISS: process.env.JWT_ISS
+  }
+}
